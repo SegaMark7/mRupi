fddc63e9e0420852e89067f84e4b0ef92c7cc05a
SegaMark <SegaMark6@gmail.com>
2024-12-05 22:41:15 +0700

    new: добавлен класс Config

---
+++ mRupi/Core/Config.cs
@@ -0,0 +1,18 @@
+﻿namespace mRupi.Core;
+
+public class Config
+{
+    public class SystemInfo
+    {
+        public CartInfo Cart { get; set; } = new CartInfo();
+        public List<byte> BiosRom { get; set; } = new List<byte>();
+        public List<byte> SoundRom { get; set; } = new List<byte>();
+    }
+
+    public class CartInfo
+    {
+        public List<byte> Rom { get; set; } = new List<byte>();
+        public List<byte> Sram { get; set; } = new List<byte>();
+        public string SramFilePath { get; set; }
+    }
+}
